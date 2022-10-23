19df29bab1db220e54e2e591e57b6b7b8cc46f75
Kara Anderson <kanderson8585@gmail.com>
2022-10-23 16:21:24 -0600

    Initial commit

---
+++ friends.js
@@ -0,0 +1,9 @@
+var friends = [23, 78, 1, 34, 90, 54, 88, 31]
+
+console.log(friends.length);
+
+ for (var i = 0; i <= 7; i++) {
+     if (friends[i] < 60) {
+         console.log(friends[i]);
+    }
+}
